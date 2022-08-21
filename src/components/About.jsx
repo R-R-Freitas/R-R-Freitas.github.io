481f716004b9458d608e80a676219214ef4634f6
R-R-Freitas <rodrigoaz8@gmail.com>
2022-08-21 13:40:36 -0300

    feat: About

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About(photo, myName, profession) {
+  const { photo, myName } = this.props;
+  return (
+    <div className='About'>
+      <img className='AboutPhoto' src={photo} alt={myName} />
+      <p className='AboutName'>{myName}</p>
+      <p className='AboutProfession'>{profession}</p>
+      <p className='AboutContent'>
+        Olá, me chamo {myName}, {profession}.
+        Aqui você pode dar uma olhada nos meus projetos e me seguir no LinkedIn e Github
+      </p>
+    </div>
+  );
+};
+
+export default About;
